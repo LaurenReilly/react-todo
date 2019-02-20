0967ac2b62f53a09ada1c5617fb2b222feba0bdc
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-02-20 14:51:57 -0500

    completed bonus

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ class Form extends Component {
         this.state = {
             todos: [
                 {
-                    todoText: "I need to do this",
+                    todoText: "Become a programmer",
                     isCompleted: false
                 },
             ],
-            inputText: "default",
+            inputText: "Lets do this!",
         }
     }
-
+//adding a new todo using the value of inputText in state
     handleSubmit(e){
         e.preventDefault();
         let newTodo = {
@@ -22,11 +22,11 @@ class Form extends Component {
         }
         this.setState({todos: [...this.state.todos, newTodo]})
     }
-
+//updating state with the input text as it changes
     handleChange(e) {
         this.setState({inputText: e.target.value})
     }
-    
+//changing isCompleted value on individual todo items when their check icon is clicked  
     toggleIsCompleted(index) {
         this.setState({
             todos: this.state.todos.map((todo, i) => {
@@ -41,26 +41,48 @@ class Form extends Component {
  
     render () {
         return (
-            <div>
-                <div>
-                    <form onSubmit={(e) => this.handleSubmit(e)}>
-                        <input type="text" value={this.state.inputText} onChange={(e) => this.handleChange(e)} />
-                        <button type="submit">SUBMIT</button>
+            <div className="m-5 m-auto" style={{width:"90%"}}>
+                <h1 className="text-center my-5">What to you need to DO?</h1>
+                <div className="m-auto" style ={{width:"90%"}}>
+                    <form className="m-auto rounded d-flex flex-column align-content-center justify-content-center" style ={{width:"300px", border:"5px solid green"}} onSubmit={(e) => this.handleSubmit(e)}>
+                        <input type="text" className="m-2" style ={{width:"270px"}} value={this.state.inputText} onChange={(e) => this.handleChange(e)} />
+                        <button className="m-2 btn btn-success btn-lg" type="submit">SUBMIT</button>
                     </form>
                 </div>
                 <div>
-                    <h2>Todo</h2>
-                    <div>
+                    <h2 className="">Todo</h2>
+                    <div className="m-2">
                         {this.state.todos.map((todo, index) => {
-                            return (
-                                <TodoList key={index} 
+                            if (!todo.isCompleted) {
+                                return (
+                                    <TodoList key={index} 
                                           isCompleted={todo.isCompleted} 
                                           text={todo.todoText} 
                                           toggle={() => this.toggleIsCompleted(index)} />
-                            )
+                                )
+                            } else {
+                                return null;
+                            }
+
                             }
                         )}
-                        
+                    </div>
+                    <h2>Todone</h2>
+                    <div className="m-2 text-muted">
+                        {this.state.todos.map((todo, index) => {
+                            if (todo.isCompleted) {
+                                return (
+                                    <TodoList key={index} 
+                                          isCompleted={todo.isCompleted} 
+                                          text={todo.todoText} 
+                                          toggle={() => this.toggleIsCompleted(index)} />
+                                )
+                            } else {
+                                return null;
+                            }
+
+                            }
+                        )}
                     </div>
                 </div>
             </div>
@@ -71,9 +93,9 @@ class Form extends Component {
 
 let TodoList = (props) => {
     return (
-        <div className="d-flex" style={{width:"60%"}}>
+        <div className="d-flex m-2 " style={{width:"60%"}}>
             <h3 className="mx-2" style={{}}>{props.text}</h3>
-            {props.isCompleted ? <i className="far fa-check-square" onClick={() => props.toggle()}></i> : <i className="far fa-square" onClick={() => props.toggle()}></i> } 
+            {props.isCompleted ? <i className="far fa-2x fa-check-square" onClick={() => props.toggle()}></i> : <i className="far fa-2x fa-square" onClick={() => props.toggle()}></i> } 
         </div>
         
     )
